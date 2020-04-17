cdfe104d00bfdc099ff8f08410a15ddc754ba9eb
zgg548 <35549898+zgg548@users.noreply.github.com>
2020-04-17 23:05:02 +0800

    add

--- src/cddl/error_rate_based/ddm.py
+++ src/cddl/error_rate_based/ddm.py
@@ -1,6 +1,10 @@
 from skmultiflow.drift_detection.ddm import DDM
 
 class DDM(DDM):
+    """
+    Come from skmultiflow
+
+    """
 
     def __init__(self, min_num_instances=30, warning_level=2.0, out_control_level=3.0):
         super().__init__(min_num_instances, warning_level, out_control_level)
