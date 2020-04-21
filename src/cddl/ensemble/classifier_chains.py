9ed385fe52329af43906850adf72a13bc2a1bb4b
zgg548 <best123xy@126.com>
2020-04-21 11:30:15 +0800

    update classifier_chains.py

--- src/cddl/ensemble/classifier_chains.py
+++ src/cddl/ensemble/classifier_chains.py
@@ -1,12 +1,17 @@
-from sklearn.linear_model import LogisticRegression
-from skmultiflow.meta.classifier_chains import ClassifierChain
+from skmultiflow.meta.classifier_chains import *
 
 
 class ClassifierChain(ClassifierChain):
 
-    """
-    come from skmultiflow
-    """
+    def __init__(self, base_estimator=LogisticRegression(), order=None, random_state=None):
+        super().__init__(base_estimator, order, random_state)
+
+class ProbabilisticClassifierChain(ProbabilisticClassifierChain):
 
     def __init__(self, base_estimator=LogisticRegression(), order=None, random_state=None):
         super().__init__(base_estimator, order, random_state)
+
+class MonteCarloClassifierChain(MonteCarloClassifierChain):
+
+    def __init__(self, base_estimator=LogisticRegression(), M=10, random_state=None):
+        super().__init__(base_estimator, M, random_state)
